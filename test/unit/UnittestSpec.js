501d16b9e33a25055aa863bee42ded4aaa7b679f
Christoph Eberl <office@cebra-webdesign.at>
2015-03-25 00:34:29 +0100

    setup test environment, and coverage, first test on success

---
+++ test/unit/UnittestSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+describe('Test the business logic of the waves animation', function() {
+  var element, scope, item;
+
+  beforeEach(module('ClockApp'));
+ 
+  it('should work', function(){
+    expect(1).toBe(1);
+  });
+
+});
